88a7df519687105449e6d6b732486fae1b0b63ac
azra <91065112+muhammadazra4503@users.noreply.github.com>
2023-11-30 09:44:01 +0700

    Initial Commit

--- app/src/main/java/com/dicoding/cocokin/ui/login/LoginActivity.kt
+++ app/src/main/java/com/dicoding/cocokin/ui/login/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.dicoding.cocokin
+package com.dicoding.cocokin.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dicoding.cocokin.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
